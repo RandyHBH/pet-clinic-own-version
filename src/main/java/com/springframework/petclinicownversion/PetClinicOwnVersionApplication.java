41a2e74dacc76a4560e296d993decc04edd4dd01
Randy Hector Bartumeu Huergo <randy@aleph.engineering>
2018-11-21 18:42:17 -0100

    Add initial files

---
+++ src/main/java/com/springframework/petclinicownversion/PetClinicOwnVersionApplication.java
@@ -0,0 +1,12 @@
+package com.springframework.petclinicownversion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetClinicOwnVersionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetClinicOwnVersionApplication.class, args);
+    }
+}
